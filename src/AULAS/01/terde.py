234630d66d2203c2d8104d0a1da1d74cccd04776
Evandro J.R. Silva <ejrs.profissional@gmail.com>
2025-09-08 19:51:07 -0300

    aula 01

---
+++ src/AULAS/01/terde.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+# Inserindo um rótulo (label) na janela
+message = tk.Label(root, text="Hello, World!")
+message.pack()
+
+root.mainloop()
+
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.mainloop()
